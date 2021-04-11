ff593e383a06bd3ed6544b077034a48875be512d
Danny Gavin <homevpn14@gmail.com>
2021-04-10 20:56:18 -0700

    Added the project cards and more styling

--- js/app.js
+++ js/app.js
@@ -4,22 +4,25 @@
 $.ajax("https://spreadsheets.google.com/feeds/list/10vCKRW3pH7xYwJO6E_0_ufhaKcfvna8j6HUz9hPKhbA/1/public/full?alt=json")
 //.then for when the data arrives
 .then((data) => {
-    console.log(data);
-
+    console.log(data)
     //Map over the data to generate a simpler/prettier data set
     const projects = data.feed.entry.map((item) => {
         return {
             description: item.gsx$description.$t,
             giturl: item.gsx$giturl.$t,
             image: item.gsx$image.$t,
-            liveurl: item.gsx$liveurl,
-            project: item.gsx$project.$t
+            liveurl: item.gsx$liveurl.$t,
+            projectName: item.gsx$project.$t
         }
     })
-    console.log(projects)
-    // projects.forEach((project) => {
-    //     console.log(project.description);
-    // })
+    
+    const $main = $("main");
+    projects.forEach((project) => {
+        const $div = $("<div>");
+        $div.html(`<project-card projectName="${project.projectName}" img="${project.image}" description="${project.description}"></project-card>`);
+        $main.append($div);
+    })
+    
     
 })
 .catch((error) => {
--- js/webcomponents.js
+++ js/webcomponents.js
@@ -4,11 +4,67 @@
 // Web Components Classes Here
 //////////////////////////////////
 //PROJECT CARD COMPONENT
+
 class ProjectCard extends HTMLElement {
     constructor(){
         super()
+        
+        this.attachShadow({mode: "open"})
+        
+        const style = `
+        <style>
+            .boxcard {
+                width: 350px;
+                text-align: center;
+                border: 1px solid;
+                border-radius: 9px;
+                margin-top: 14px;
+                margin-bottom: 28px;
+                background-color: #c150ff;
+            }
+            .boxcard:hover {
+                box-shadow: 0 0 10px #ccc;
+            }
+            img {
+                width: 350px;
+            }
+            p {
+             margin: 0;
+             padding: 10px;
+             color: white; 
+             text-align: center;
+            }
+            #dropdown {
+                border-top:1px solid;
+                transition: all 0.5s;
+            }
+            #dropdown:hover .dropdown-content{
+                display: block;
+            }
+            .dropdown-content {
+                display: none;
+                padding-top: 5px;
+            }
+        </style>
+        `
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        ${style}
+        <div class="boxcard">
 
-        this.innerHTML = `<h1>Hello World</h1>`
+        <h2>${this.getAttribute("projectName")}</h2>
+        <img src=${this.getAttribute("img")}>
+        <p>${this.getAttribute("description")}</p>
+        <div id="dropdown">
+        <div class="dropdown-content">
+            <a>Check it out on Github</a>
+        </div>
+        <i class="fas fa-chevron-down"></i>
+        </div>
+
+        </div>
+        `
     }
 }
 
